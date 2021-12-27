52fc00d0e3cefb1423921a01844ffeae899e72e7
Katsuke <cheemsdog569@outlook.com>
2021-12-27 23:33:28 +0500

    Structured dummy.js

--- commands/dummy.js
+++ commands/dummy.js
@@ -1,6 +1,4 @@
 const CustomEmbed = require("../functions/CostumEmbed");
-const CustomCollector = require("../functions/CustomCollector");
-const {MessageButton} = require("discord.js");
 const {SlashCommandBuilder} = require("@discordjs/builders");
 
 module.exports = {
@@ -13,15 +11,7 @@ module.exports = {
     async execute(interaction) {
 
         // Code here...
-        const embed = new CustomEmbed({title : "Mmmmmm"});
-        const btn = new MessageButton({label:"E ?", style:"SUCCESS", customId:"e"});
-        const nah = new MessageButton({label:"N ?", style:"SUCCESS", customId:"n"});
-        const wtf = new MessageButton({label:"wtf ?", style:"SUCCESS", customId:"wtf"});
-        const msg = new CustomCollector(interaction, [embed], [btn]);
-        msg.setCustomRows([[wtf, btn],[nah]]);
-        msg.start(async i => {
-            await i.reply("Monke !");
-        });
+        await interaction.reply("Helo Pepo !");
 
     }
 }
